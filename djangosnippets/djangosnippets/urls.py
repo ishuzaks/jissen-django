710fb838a1bc3fe073b4a351e3063d2721c84385
ishuzaks <k.suzuki512+develop@gmail.com>
2022-05-15 09:54:13 +0900

    Add Hello World.

--- djangosnippets/djangosnippets/urls.py
+++ djangosnippets/djangosnippets/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from snippets.views import top
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path("", top, name="top"),
+    path("admin/", admin.site.urls),
 ]
